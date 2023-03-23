20502d5b9a25bba547cb03291a52f08b04d1be14
godfrey <gofreyiibritannia@gmail.com>
2023-03-23 15:42:34 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,187 @@
+const jhttp = new XMLHttpRequest();
+
+const pokemon = new Array(
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Caterpie",
+  "Metapod",
+  "Butterfree",
+  "Weedle",
+  "Kakuna",
+  "Beedrill",
+  "Pidgey",
+  "Pidgeotto",
+  "Pidgeot",
+  "Rattata",
+  "Raticate",
+  "Spearow",
+  "Fearow",
+  "Ekans",
+  "Arbok",
+  "Pikachu",
+  "Raichu",
+  "Sandshrew",
+  "Sandslash",
+  "Nidorina",
+  "Nidoqueen",
+  "Nidoran-f",
+  "Nidorino",
+  "Nidoking",
+  "Clefairy",
+  "Clefable",
+  "Vulpix",
+  "Ninetales",
+  "Jigglypuff",
+  "Wigglytuff",
+  "Zubat",
+  "Golbat",
+  "Oddish",
+  "Gloom",
+  "Vileplume",
+  "Paras",
+  "Parasect",
+  "Venonat",
+  "Venomoth",
+  "Diglett",
+  "Dugtrio",
+  "Meowth",
+  "Persian",
+  "Psyduck",
+  "Golduck",
+  "Mankey",
+  "Primeape",
+  "Growlithe",
+  "Arcanine",
+  "Poliwag",
+  "Poliwhirl",
+  "Poliwrath",
+  "Abra",
+  "Kadabra",
+  "Alakazam",
+  "Machop",
+  "Machoke",
+  "Machamp",
+  "Bellsprout",
+  "Weepinbell",
+  "Victreebel",
+  "Tentacool",
+  "Tentacruel",
+  "Geodude",
+  "Graveler",
+  "Golem",
+  "Ponyta",
+  "Rapidash",
+  "Slowpoke",
+  "Slowbro",
+  "Magnemite",
+  "Magneton",
+  "Doduo",
+  "Dodrio",
+  "Seel",
+  "Dewgong",
+  "Grimer",
+  "Muk",
+  "Shellder",
+  "Cloyster",
+  "Gastly",
+  "Haunter",
+  "Gengar",
+  "Onix",
+  "Drowzee",
+  "Hypno",
+  "Krabby",
+  "Kingler",
+  "Voltorb",
+  "Electrode",
+  "Exeggcute",
+  "Exeggutor",
+  "Cubone",
+  "Marowak",
+  "Hitmonlee",
+  "Hitmonchan",
+  "Lickitung",
+  "Koffing",
+  "Weezing",
+  "Rhyhorn",
+  "Rhydon",
+  "Chansey",
+  "Tangela",
+  "Kangaskhan",
+  "Horsea",
+  "Seadra",
+  "Goldeen",
+  "Seaking",
+  "Staryu",
+  "Starmie",
+  "Scyther",
+  "Jynx",
+  "Electabuzz",
+  "Magmar",
+  "Pinsir",
+  "Tauros",
+  "Magikarp",
+  "Gyarados",
+  "Lapras",
+  "Ditto",
+  "Eevee",
+  "Vaporeon",
+  "Jolteon",
+  "Flareon",
+  "Porygon",
+  "Omanyte",
+  "Omastar",
+  "Kabuto",
+  "Kabutops",
+  "Aerodactyl",
+  "Snorlax",
+  "Articuno",
+  "Zapdos",
+  "Moltres",
+  "Dratini",
+  "Dragonair",
+  "Dragonite",
+  "Mewtwo",
+  "Mew",
+  "Farfetchd",
+  "Mr-mime"
+);
+
+const pokeRandom = pokemon[Math.floor(Math.random() * pokemon.length)];
+const pokeGet = pokeRandom.toLowerCase();
+
+document.querySelector(".reset").addEventListener("click", () => {
+  location.reload();
+});
+
+const getApi = () => {
+  jhttp.onload = function () {
+    const response = JSON.parse(this.responseText);
+    console.log(response);
+
+    //getName
+    const pokeTitle = document.querySelector(".pokemon-name");
+    pokeTitle.innerText = `${response.species.name}`;
+
+    //getPicture
+    const pokePic = document.querySelector("img");
+    pokePic.src = `${response.sprites.front_default}`;
+
+    const result = document.querySelector(".result");
+    result.style = "display: block;";
+
+    const desc = document.querySelector(".desc");
+    desc.style = "display: block";
+
+    const pokeRecieved = document.querySelector(".recieved");
+    pokeRecieved.innerText = `You got ${pokeGet}!`;
+  };
+  jhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${pokeGet}`);
+  jhttp.send();
+};
